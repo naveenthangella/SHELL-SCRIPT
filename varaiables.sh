69234aa240bc0c188469915315d608fff5c886f6
Naveen <naveenthangella@gmail.com>
2019-12-10 00:31:56 -0800

    a

--- varaiables.sh
+++ varaiables.sh
@@ -3,6 +3,6 @@
 Name=Naveen
 echo -e "Hello Im \e[31m $Name\e[0m"
 echo -e "\nToday is \e[32m $(date +%A)\e[0m"
-echo -e "\nAnd the Date is\e[33m $(date +%B) $(date +%d) $(date +%Y)"
-read number
-echo $number
+echo -e "\nAnd the Date is\e[33m $(date +%B) $(date +%d) $(date +%Y)\e[0m"
+read -p "enter your phone number" number
+echo -e"entered number is \e[46m$number"
