9bc20e4c38f10bfbb6f262be66affde0f5404af0
Naveen <naveenthangella@gmail.com>
2019-12-10 01:33:24 -0800

    a

--- case.sh
+++ case.sh
@@ -1,4 +1,27 @@
 #!bin/bash
 
+action = $1
 read -p "enter two numbers" a b
-echo $(($a+$b))
+
+case $action in
+ADD)
+    echo $(($a+$b))
+    exit 0
+    ;;
+Multiply)
+    echo $(($a*$b))
+    exit 0
+    ;;
+Divide)
+    echo $(($a/$b))
+    exit 0
+    ;;
+substract)
+    echo $(($a*$b))
+    exit 0
+    ;;
+*)
+    echo invalid input
+    exit 1
+    ;;
+esac
