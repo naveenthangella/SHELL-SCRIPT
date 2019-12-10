6ff6235fa313f3964e583692db8435c21c905358
Naveen <naveenthangella@gmail.com>
2019-12-10 00:43:46 -0800

    Demo on using a variable, command substitution and reading the value.

--- varaiables.sh
+++ varaiables.sh
@@ -1,8 +1,11 @@
 #!bin/bash
-
-Name=Naveen
-echo -e "Hello Im \e[31m $Name\e[0m"
+## variable Declaration
+Name=Naveen 
+## accessing the variable
+echo -e "Hello Im \e[31m $Name\e[0m" 
+## command substitution syntax: $(command)
 echo -e "\nToday is \e[32m $(date +%A)\e[0m"
 echo -e "\nAnd the Date is\e[33m $(date +%B) $(date +%d) $(date +%Y)\e[0m"
+## taking input from terminal
 read -p "enter your phone number" number
 echo -e "entered number is \e[46m$number\e[0m"
