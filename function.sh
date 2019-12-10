a907cc802969ab301b9e2dd2258ff34e553b0bf8
Naveen <naveenthangella@gmail.com>
2019-12-10 01:16:19 -0800

    a

--- function.sh
+++ function.sh
@@ -7,5 +7,14 @@ ADD(){
 Multiplication(){
     echo $(($a*$b))
 }
+read -p "enter + for ADD or * for Multiplication" action 
+if ["$action" == "+"]; then
 ADD
+exit 0
+elif ["$action" == "*"]; then
 Multiplication
+exit 0
+else
+echo invalid input
+exit 1
+fi
