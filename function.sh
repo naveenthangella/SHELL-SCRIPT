ca4ef202ab3bf77c6dd74cdc8ae8a99c398266a2
Naveen <naveenthangella@gmail.com>
2019-12-10 01:25:10 -0800

    sample functions with if else condition

--- function.sh
+++ function.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+
 a=5
 b=10
 ADD(){
@@ -8,13 +9,17 @@ Multiplication(){
     echo $(($a*$b))
 }
 read -p "enter + for ADD or * for Multiplication" action 
+
 if [ "$action" == "+" ]; then
 ADD
 exit 0
+
 elif [ "$action" == "*" ]; then
 Multiplication
 exit 0
+
 else
 echo invalid input
 exit 1
+
 fi
