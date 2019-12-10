c79e73e58f64079f2c40bc7dead3fa309d18c8dc
Naveen <naveenthangella@gmail.com>
2019-12-09 23:45:55 -0800

    printing sample message with different colors

--- print.sh
+++ print.sh
@@ -1,1 +1,7 @@
+#!bin/bash
+echo -e "\e[31m Hello world"
+echo -e "\e[32m Hello world"
 echo -e "\e[33m Hello world"
+echo -e "\e[34m Hello world"
+echo -e "\e[35m Hello world"
+echo -e "\e[36m Hello world"
