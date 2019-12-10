4a8a562e296a0dcbe98e9306d3a30af2453326f6
Naveen <naveenthangella@gmail.com>
2019-12-09 23:48:21 -0800

    checking background colors

--- print.sh
+++ print.sh
@@ -2,6 +2,6 @@
 echo -e "\e[31m Hello world"
 echo -e "\e[32m Hello world"
 echo -e "\e[33m Hello world"
-echo -e "\e[34m Hello world"
+echo -e "\e[34;42m Hello world"
 echo -e "\e[35m Hello world"
 echo -e "\e[36m Hello world"
