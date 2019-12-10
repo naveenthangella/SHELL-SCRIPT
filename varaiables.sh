4cc54ab84f0ad3a918b52f71c013c8be7b5c8964
Naveen <naveenthangella@gmail.com>
2019-12-10 00:19:51 -0800

    a

--- varaiables.sh
+++ varaiables.sh
@@ -2,5 +2,5 @@
 
 Name=Naveen
 echo -e "Hello \n\tIm \e[31m $Name"
-echo -e "Today is \n\tIm \e[31m $(date +%A)"
-echo -e "And the Date is \n\tIm \e[31m $(date +%d) $(date +%B) $(date +%Y)"
+echo -e "Today is \n\t \e[32m $(date +%A)"
+echo -e "And the Date is \n\t \e[33m $(date +%B) $(date +%D) $(date +%Y)"
