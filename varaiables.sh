12d32246995ea734c34be824bbd6cab9998db99e
Naveen <naveenthangella@gmail.com>
2019-12-10 00:28:23 -0800

    a

--- varaiables.sh
+++ varaiables.sh
@@ -4,3 +4,5 @@ Name=Naveen
 echo -e "Hello Im \e[31m $Name\e[0m"
 echo -e "\nToday is \e[32m $(date +%A)\e[0m"
 echo -e "\nAnd the Date is\e[33m $(date +%B) $(date +%d) $(date +%Y)"
+read number
+echo $number
